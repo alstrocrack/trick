bee25c79b9a6a3d7e6e88d00d4402034c16a8161
YutoUrushima <yuto259233@gmail.com>
2022-12-07 22:33:38 +0900

    Add add_new_request method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
     @user = User.first
   end
+
+  def add_new_request
+    raise "NULL From" if params[:from].blank?
+    # puts "Hello"
+    redirect_to "/"
+  end
 end
