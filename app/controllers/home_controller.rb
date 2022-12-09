12f9e88bf1114d5601627ac85f6166f9a3529575
YutoUrushima <yuto259233@gmail.com>
2022-12-09 21:32:06 +0900

    Add prettierrc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,7 @@ class HomeController < ApplicationController
   end
 
   def add_new_request
-    if params[:from].blank?
-      raise ApplicationError.new(ErrorCode::E1000, ErrorMessage::Param)
-    end
+    raise ApplicationError.new(ErrorCode::E1000, ErrorMessage::Param) if params[:from].blank?
     # puts "Hello"
     redirect_to "/"
   end
