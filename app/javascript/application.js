f478b232375138dda58a39a79ee57a7f0faa4138
YutoUrushima <yuto259233@gmail.com>
2022-12-12 21:21:58 +0900

    prettier application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
 // Modules to add on your own
 import { Ajax } from "./ajax";
-const ajax = new Ajax('post', "/", "{}", null, null);
+const ajax = new Ajax("post", "/", "{}", null, null);
 ajax.try();
