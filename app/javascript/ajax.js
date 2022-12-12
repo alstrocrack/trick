37df5ead808d00194c34bb71ec1d493cf4bc6ba9
YutoUrushima <yuto259233@gmail.com>
2022-12-12 21:20:40 +0900

    Add prettier to JavaScript

--- app/javascript/ajax.js
+++ app/javascript/ajax.js
@@ -1,5 +1,5 @@
 export class Ajax {
-  constructor(method, url, data, success, fail){
+  constructor(method, url, data, success, fail) {
     this.method = method;
     this.url = url;
     this.data = data;
@@ -11,13 +11,16 @@ export class Ajax {
       type: this.method,
       url: this.url,
       data: this.data,
-    }).done((result) => {
-      console.log(result);
-    }).fail((result) => {
-      console.log(result);
-    }).always(() => {
-      console.log('hi');
     })
+      .done((result) => {
+        console.log(result);
+      })
+      .fail((result) => {
+        console.log(result);
+      })
+      .always(() => {
+        console.log("hi");
+      });
   }
   try() {
     console.log(this.method);
