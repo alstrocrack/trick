317e9038f5f03a4142d3bd4db83c11d45e772d2c
YutoUrushima <yuto259233@gmail.com>
2022-12-14 21:36:34 +0900

    Add endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post "/home/add_new_request", controller: :home, action: :add_new_request
+  post "/api/index", controller: :api, action: :index
+  get "/api/index", controller: :api, action: :index
   root to: "home#index"
 end
