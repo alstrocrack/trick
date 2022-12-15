2343e0c006239db293f127f168a3b7d575e12dc3
YutoUrushima <yuto259233@gmail.com>
2022-12-15 22:09:13 +0900

    Fix register rule WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,21 @@
+require "json"
+
 class HomeController < ApplicationController
   def index
-    @user = UserAccount.first
     @requests = Request.where(user_id: @user.id)
   end
 
-  def add_new_request
-    raise ApplicationError.new(ErrorCode::E1000, ErrorMessage::Param) if params[:from].blank?
-    request =
-      Request.new(
-        from_address: params[:from],
-        response_header: params[:header],
-        response_body: params[:body]
-      )
-    request.save!
-    redirect_to "/"
+  def add
+    if Request.valid?(params)
+      request =
+        Request.new(
+          user_id: @user.id,
+          from_address: params[:from],
+          response_header: params[:header].to_json,
+          response_body: params[:body].to_json
+        )
+      request.save!
+      redirect_to "/"
+    end
   end
 end
