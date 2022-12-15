8e6947d57d8fff30edcab67ba54d0d07509ba81f
YutoUrushima <yuto259233@gmail.com>
2022-12-15 22:08:35 +0900

    Change route & Add validation

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,8 @@
 class ErrorCode
-  E1000 = 1000
+  E1001 = 1001
+  E1002 = 1002
 end
 class ErrorMessage
-  Param = "Invalid params."
+  Param = "Invalid params." #1001
+  From = "Invalid From." #1002
 end
