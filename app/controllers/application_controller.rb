7de01652bdf8b9743a1fe59c050127cc920ba0f8
YutoUrushima <yuto259233@gmail.com>
2022-12-16 21:48:38 +0900

    Fix point cause error & Add rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action { @user = UserAccount.first }
+
+  rescue_with ApplicationError, with: ->(e) {}
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,2 @@
 class Request < ApplicationRecord
-  def self.valid?(input)
-    raise ApplicationError.new(ErrorCode::E1002, ErrorMessage::From) if input[:from].blank?
-    return true
-  end
 end
