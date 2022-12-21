e1409970fb1636b7770d80a2594c460162a12968
YutoUrushima <yuto259233@gmail.com>
2022-12-21 22:38:29 +0900

    Add Ajax function

--- app/javascript/ajax.js
+++ app/javascript/ajax.js
@@ -1,14 +1,28 @@
-export const getXhr = (url, data, success, fail) => {
+export const getXhr = (url, success, fail) => {
   $.ajax(url, {
     type: "get",
+  })
+    .done((result) => {
+      console.log(result);
+      success(result);
+    })
+    .fail((result) => {
+      console.log(result);
+      // fail(result);
+    });
+};
+
+export const postXhr = (url, data, success, fail) => {
+  $.ajax(url, {
+    type: "post",
     data: data,
   })
     .done((result) => {
       console.log(result);
-      success();
+      success(result);
     })
     .fail((result) => {
       console.log(result);
-      fail();
+      // fail(result);
     });
 };
