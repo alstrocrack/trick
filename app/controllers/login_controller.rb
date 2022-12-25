dd330814a2d402a9d2ec0bb40264db1128b7ba3c
YutoUrushima <yuto259233@gmail.com>
2022-12-25 18:34:13 +0900

    Add login method

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,11 @@ class LoginController < ApplicationController
   def index
   end
 
-  def login
+  def authenticate
+    execute("/login", "login", email, password) do |parameters|
+      if parameters[:email].blank? || parameters[:password].blank?
+        raise ApplicationError.new(ErrorCode::E1004, ErrorMessage::LackOfParameters)
+      end
+    end
   end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,9 +2,11 @@ class ErrorCode
   E1001 = 1001
   E1002 = 1002
   E1003 = 1003
+  E1004 = 1004
 end
 class ErrorMessage
   Param = "Invalid params." #1001
   From = "Invalid From." #1002
   LimitRequetsExceeds = "The maximum number of requests that can be registered has been reached." #1003
+  LackOfParameters = "Please enter both email and password." #1004
 end
