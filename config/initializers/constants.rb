4b51a56d9b8105e451d16b4f21cffec52153b67f
YutoUrushima <yuto259233@gmail.com>
2022-12-25 18:24:49 +0900

    Add validation not to exceecd limit

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,10 @@
 class ErrorCode
   E1001 = 1001
   E1002 = 1002
+  E1003 = 1003
 end
 class ErrorMessage
   Param = "Invalid params." #1001
   From = "Invalid From." #1002
+  LimitRequetsExceeds = "The maximum number of requests that can be registered has been reached." #1003
 end
