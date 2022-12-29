c305a50cc349c5d8e320218deb7cae78c75893f4
YutoUrushima <yuto259233@gmail.com>
2022-12-29 18:47:01 +0900

    Add authentication logic

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,12 +1,17 @@
+require "digest"
+
 class LoginController < ApplicationController
   def index
   end
 
   def authenticate
     execute("/login", "login", email, password) do |parameters|
-      if parameters[:email].blank? || parameters[:password].blank?
-        raise ApplicationError.new(ErrorCode::E1004, ErrorMessage::LackOfParameters)
-      end
+      raise ApplicationError.new(ErrorCode::E1004, ErrorMessage::LackOfParameters) if parameters[:email].blank? || parameters[:password].blank?
+      user_account = UserAccout.find_by(email: parameters[:email])
+      raise ApplicationError.new(ErrorCode::E1005, ErrorMessage::NonExistentUsers) if user_account.nil?
+      password_hash = Digest::SHA256.digest(parameters[:password].strip)
+      raise ApplicationError.new(ErrorCode::E1006, ErrorMessage::InvalidPassword) unless user_account.authenticate?(password_hash)
+      # make session.
     end
   end
 end
