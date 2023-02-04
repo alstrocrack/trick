f6c5861e6458ea56d130f895cbf8092cb6fa1836
YutoUrushima <yuto259233@gmail.com>
2023-02-04 21:17:03 +0900

    Add post_execute YARD Docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # Provide strong parameter mechanism and exception handling when executing POST method
+  # @param [String] fail_redirect_path Redirects to when processing fails
+  # @param [String] group Specify a group of POST parameters
+  # @param [Array] *filters Specify which of the POST parameters are permitted
   def post_execute(fail_redirect_path, group, *filters)
     parameters = params.require(group).permit(filters)
     yield(parameters)
