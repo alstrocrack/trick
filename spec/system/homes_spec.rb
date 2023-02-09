92e387a163b419e4f379547611ca3e07d8432fda
YutoUrushima <yuto259233@gmail.com>
2023-02-09 19:58:26 +0900

    Fix home system home spec

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -1,12 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "Homes", type: :system do
-  context "as a registered user" do
-    it "responds successfully and return correct requests" do
-      user_account = FactoryBot.create(:user_account)
-      sign_in_with(user_account.email, "password")
+  it "login successfully and display user_account's email" do
+    user_account = FactoryBot.create(:user_account, email: "example@example.com")
 
-      visit root_path
-    end
+    visit root_path
+    expect(page).to_not have_content "example@example.com"
+
+    sign_in_with(user_account.email, "password")
+    visit root_path
+
+    expect(page).to have_content "example@example.com"
+  end
+
+  context "as a registered user" do
   end
 end
