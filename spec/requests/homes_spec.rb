5917ea98ce8172ae2334073ffde376a86df1858c
YutoUrushima <yuto259233@gmail.com>
2023-02-09 18:59:02 +0900

    Change home controller test

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -2,9 +2,8 @@ require "rails_helper"
 
 RSpec.describe "Homes", type: :request do
   describe "GET /homes" do
-    it "works! (now write some real specs)" do
-      get root_path
-      expect(response).to have_http_status(200)
+    context "as a registered user" do
+      # user_account = FactoryBot.create(:user_account)
     end
   end
 end
