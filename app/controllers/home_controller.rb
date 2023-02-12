f6ff5db3191551af603d6d228ee389fe2fb873a1
YutoUrushima <yuto259233@gmail.com>
2023-02-12 21:03:13 +0900

    Fix to enable to add request as a guest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,12 @@ class HomeController < ApplicationController
   def add
     post_execute("/", "home", :user_id, :from, :header, :body) do |parameters|
       raise ApplicationError.new(ErrorCode::E1002, ErrorMessage::From) if parameters[:from].blank?
-      raise ApplicationError.new(ErrorCode::E1003, ErrorMessage::LimitRequetsExceeds) if @user_account.is_exceed?
-      request = Request.new(user_id: @user_account.id, from_address: parameters[:from], response_header: JSON.parse(parameters[:header]), response_body: JSON.parse(parameters[:body]))
-      request.save!
-      flash[:success] = "Successfully registerd"
+      raise ApplicationError.new(ErrorCode::E1003, ErrorMessage::LimitRequetsExceeds) if @user_account && @user_account.is_exceed?
+      header = parameters[:header].blank? ? nil : JSON.parse(parameters[:header])
+      body = parameters[:body].blank? ? nil : JSON.parse(parameters[:body])
+      request = Request.new(user_id: @user_account&.id, from_address: parameters[:from], response_header: header, response_body: body)
+      flash[:success] = "Successfully registerd" if request.save!
+      redirect_to "/"
     end
   end
 end
