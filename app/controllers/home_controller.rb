aefe3ffd9d7e4b098a7a164253d202778421cca5
YutoUrushima <yuto259233@gmail.com>
2023-02-13 20:01:34 +0900

    Fix to enable guest user to register requests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 require "json"
+require "securerandom"
 
 class HomeController < ApplicationController
   before_action :fetch_user_session
@@ -8,14 +9,26 @@ class HomeController < ApplicationController
   end
 
   def add
-    post_execute("/", "home", :user_id, :from, :header, :body) do |parameters|
-      raise ApplicationError.new(ErrorCode::E1002, ErrorMessage::From) if parameters[:from].blank?
-      raise ApplicationError.new(ErrorCode::E1003, ErrorMessage::LimitRequetsExceeds) if @user_account && @user_account.is_exceed?
-      header = parameters[:header].blank? ? nil : JSON.parse(parameters[:header])
-      body = parameters[:body].blank? ? nil : JSON.parse(parameters[:body])
-      request = Request.new(user_id: @user_account&.id, from_address: parameters[:from], response_header: header, response_body: body)
-      flash[:success] = "Successfully registerd" if request.save!
-      redirect_to "/"
+    post_execute("/", "home", :status, :from, :header, :body) do |parameters|
+      ActiveRecord::Base.transaction do
+        raise ApplicationError.new(ErrorCode::E1002, ErrorMessage::From) if parameters[:from].blank?
+        raise ApplicationError.new(ErrorCode::E1003, ErrorMessage::LimitRequetsExceeds) if @user_account && @user_account.is_exceed?
+        header = parameters[:header].blank? ? nil : JSON.parse(parameters[:header])
+        body = parameters[:body].blank? ? nil : JSON.parse(parameters[:body])
+        request = Request.new(status_code: parameters[:status].to_i, from_address: parameters[:from], response_header: header, response_body: body)
+        if @user_account
+          request.user_id = @user_account
+        elsif @guest_user
+          request.guest_session_id = @guest_user
+        else
+          session[:guest] = SecureRandom.uuid
+          guest_session = UserSession.new(value: session[:guest], status: UserSessionStatus::Temporary)
+          guest_session.save!
+          request.guest_session_id = guest_session.id
+        end
+        flash[:success] = "Successfully registerd" if request.save!
+        redirect_to "/"
+      end
     end
   end
 end
