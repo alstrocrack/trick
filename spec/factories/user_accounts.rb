ba0b3266dbaef0d0bb01ff5e7f2b90e6055c3b9a
YutoUrushima <yuto259233@gmail.com>
2023-04-05 15:39:40 +0900

    delete unnecessary column

--- spec/factories/user_accounts.rb
+++ spec/factories/user_accounts.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
   factory :user_account do
     sequence(:email) { |n| "example#{n}@example.com" }
     password_hash { Digest::SHA256.hexdigest("password") }
-    endpoint_value { "my_endpoint" }
   end
 end
