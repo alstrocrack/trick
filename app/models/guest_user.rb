1e76ecb3689a495c9e763c499cf4ebeb3b424be6
YutoUrushima <yuto259233@gmail.com>
2023-05-10 01:00:10 +0900

    Add guest user model

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -1,4 +1,9 @@
 class GuestUser
-  def initialize()
+  def initialize(session_value)
+    return UserSession.find_by(value: guest_session, status: UserSessionStatus::Temporary)
+  end
+
+  def is_exceed?
+    return Request.where(guest_id: self.id).count >= RequestCounts::Max
   end
 end
