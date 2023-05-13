1cad93136e1fd1b442bbc2af27b2a341c505ac43
YutoUrushima <yuto259233@gmail.com>
2023-05-13 23:50:59 +0900

    Fix to use api key when user uses api

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -11,7 +11,14 @@ class UserAccount < ApplicationRecord
     return self.password_hash == password_hash
   end
 
+  # NOTE: Mainly used for request registration inside applications, etc
   def get_api_key
     return ApiKey.find_by(owner_id: ApiKey.get_user_api_key(self.id)).value
+  end
+
+  # NOTE: Mainly used to respond to API inquiries
+  def self.get_api_key(user)
+    owner_id = ApiKey.get_user_api_key(UserAccount.find_by(name: user).id)
+    return ApiKey.find_by(owner_id: owner_id).value
   end
 end
