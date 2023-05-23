5685b37be2d5cdfa5a482de459c5f00f1a7eb0f0
YutoUrushima <yuto259233@gmail.com>
2023-05-23 22:08:38 +0900

    Add user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     parameters = params.permit(:id)
-    @user = UserAccount.find_by(id: parameters[:id])
+    @user_account = UserAccount.find_by(id: parameters[:id])
+    @api_key = ApiKey.find_by(owner_id: ApiKey.get_user_api_key(@user_account.id))
   end
 end
