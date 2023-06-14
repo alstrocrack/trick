eaf36fa767d7b0b21e11a8f4b7367fb2c29a9ef8
YutoUrushima <yuto259233@gmail.com>
2023-06-14 22:21:19 +0900

    Fix spec according to Rubocop & Add some Rubocop config

--- spec/models/user_account_spec.rb
+++ spec/models/user_account_spec.rb
@@ -1,19 +1,21 @@
 require "rails_helper"
 
 RSpec.describe UserAccount, type: :model do
-  let(:user) { FactoryBot.create(:user_account) }
+  let(:user) { create(:user_account) }
 
   describe "register multiple requests" do
     subject { user.is_exceed? }
 
     context "when user_account have four requests" do
-      before { FactoryBot.create_list(:request, 4) }
-      it { is_expected.to eq(false) }
+      before { create_list(:request, 4) }
+
+      it { is_expected.to be(false) }
     end
 
     context "when user_account have five requests" do
-      before { FactoryBot.create_list(:request, 5) }
-      it { is_expected.to eq(false) }
+      before { create_list(:request, 5) }
+
+      it { is_expected.to be(false) }
     end
   end
 
@@ -22,12 +24,14 @@ RSpec.describe UserAccount, type: :model do
 
     context "when the correct password is entered" do
       let(:password) { "password" }
-      it { is_expected.to eq(true) }
+
+      it { is_expected.to be(true) }
     end
 
     context "when the wrong password is entered" do
       let(:password) { "invalid_password" }
-      it { is_expected.to eq(false) }
+
+      it { is_expected.to be(false) }
     end
   end
 end
