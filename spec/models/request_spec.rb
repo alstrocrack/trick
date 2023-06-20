ee5d789e6ab89982543e04c7f6174a968d601be8
YutoUrushima <yuto259233@gmail.com>
2023-06-20 22:42:39 +0900

    Add first request spec

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:request) { create(:request) }
+  describe "#validate_request" do
+    context "when valid request" do
+      it "doesn't raise ApplicationError" do
+        expect { request.validate_request }.to_not raise_error
+      end
+    end
+
+    context "when invalid status_code" do
+      let(:request) { create(:request, status_code: "invalid_status_code") }
+      it "raises Application Error" do
+        expect { request.validate_request }.to raise_error(ApplicationError)
+      end
+    end
+
+    context "when blank name" do
+      let(:request) { create(:request, name: nil) }
+      it "raises Application Error" do
+        expect { request.validate_request }.to raise_error(ApplicationError)
+      end
+    end
+  end
 end
