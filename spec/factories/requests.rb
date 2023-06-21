55442daa74fc0a41bb0d3bed2d8201c94354e5b2
YutoUrushima <yuto259233@gmail.com>
2023-06-21 23:13:34 +0900

    Add some specs

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :request do
-    sequence(:name) { |n| "trick#{n}" }
+    sequence(:name) { |n| "trick-test-#{n}" }
     status_code { 200 }
     response_header { RegisteringRequest.build_registering_request_header("key1", "key2", "key3", "value1", "value2", "value3") }
     response_body { { text: "text" } }
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -23,4 +23,33 @@ RSpec.describe Request, type: :model do
       end
     end
   end
+
+  describe "#validate_user_request" do
+    let(:user) { create(:user_account) }
+    let(:request) { create(:request, user_id: user.id) }
+    let(:request_exec) { request.validate_user_request(user) }
+
+    xcontext "when valid request" do
+      let(:request) { create(:request, user_id: user.id) }
+      it "doesn't raise ApplicationError" do
+        expect { request.reload.validate_user_request(user) }.to_not raise_error
+      end
+    end
+
+    context "when duplicate request exists" do
+      let!(:duplicate_request) { create(:request, user_id: user.id, name: "dup-request") }
+      let(:request) { create(:request, user_id: user.id, name: "dup-request") }
+
+      it "raises Application Error" do
+        expect { request_exec }.to raise_error(ApplicationError)
+      end
+    end
+
+    context "when 5 or more requests" do
+      let!(:multiple_request) { create_list(:request, 5, user_id: user.id) }
+      it "raises Application Error" do
+        expect { request_exec }.to raise_error(ApplicationError)
+      end
+    end
+  end
 end
