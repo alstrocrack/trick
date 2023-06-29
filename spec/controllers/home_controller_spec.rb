5157cbd320a35c23feab0769ea28f7e4378dc0b9
YutoUrushima <yuto259233@gmail.com>
2023-06-30 01:28:12 +0900

    Fix home controller request spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,39 @@
 require "rails_helper"
 
 RSpec.describe HomeController, type: :controller do
+  let(:request_params) do
+    {
+      name: "trick",
+      status: 200,
+      key1: "x-abc-key",
+      key2: "x-def-key",
+      key3: "x-ghi-key",
+      value1: "abc",
+      value2: "def",
+      value: "ghi",
+      response_body: {
+        text: "text"
+      }
+    }
+  end
+
+  let(:invalid_request_params) do
+    {
+      name: nil,
+      status: 200,
+      key1: "x-abc-key",
+      key2: "x-def-key",
+      key3: "x-ghi-key",
+      value1: "abc",
+      value2: "def",
+      value: "ghi",
+      response_body: {
+        text: "text"
+      }
+    }
+  end
+  let(:request) { create(:request) }
+
   describe "#index" do
     it "responds successfully" do
       get :index
@@ -8,18 +41,35 @@ RSpec.describe HomeController, type: :controller do
     end
   end
 
-  describe "#add" do
-    it "responds successfully" do
-      post :create, params: { name: "trick", key1: "X-api-key", value1: "abcdef", status: 200, response_body: { text: "text" } }
-      expect(response).to have_http_status "302"
+  describe "#create" do
+    context "when valid params" do
+      it "redirects to the home page" do
+        post :create, params: request_params
+        expect(response).to have_http_status "302"
+      end
+    end
+
+    context "when invalid params" do
+      it "redirects to the home page" do
+        post :create, params: request_params
+        expect(response).to have_http_status "302"
+      end
     end
   end
 
-  describe "#delete" do
-    before { Request.create!(id: 1, name: "trick", response_header: { "X-api-key": "abcdef" }, status_code: 200, response_body: { text: "text" }) }
-    it "responds successfully" do
-      delete :destroy, params: { id: 1 }
-      expect(response).to have_http_status "302"
+  describe "#destroy" do
+    context "when valid request" do
+      it "redirects to the home page" do
+        delete :destroy, params: { id: request.id }
+        expect(response).to have_http_status "302"
+      end
+    end
+
+    context "when valid request" do
+      it "redirects to the home page" do
+        delete :destroy, params: { id: -1 }
+        expect(response).to have_http_status "302"
+      end
     end
   end
 end
