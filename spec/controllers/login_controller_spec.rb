31ef7159e84b5cb354e8aba411a12cb05cc18f3a
YutoUrushima <yuto259233@gmail.com>
2023-07-01 00:07:19 +0900

    Fix LoginController spec

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe LoginController, type: :controller do
-  let(:user_account) { FactoryBot.create(:user_account) }
-  let(:user_session) { FactoryBot.create(:user_session, user_id: user_account.id) }
+  let(:user) { create(:user_account) }
+  let(:params) { { email: "example@example.com", password: "example" } }
 
   context "as an guest user" do
     it "responds successfully" do
@@ -11,7 +11,7 @@ RSpec.describe LoginController, type: :controller do
     end
 
     it "responds redirect when we send request to /create" do
-      post :create, params: { email: "example@example.com", password: "example" }
+      post :create, params: params
       expect(response).to have_http_status "302"
     end
 
@@ -22,7 +22,11 @@ RSpec.describe LoginController, type: :controller do
   end
 
   context "as an logined user" do
-    before { session[:user] = user_session.value }
+    let!(:user_session) { create(:user_session, user_id: user.id) }
+    before do
+      session[:user_id] = user.id
+      session[:user_token] = "exmaple-session-value"
+    end
 
     it "responds redirect when we visit /login page" do
       get :index
@@ -30,7 +34,7 @@ RSpec.describe LoginController, type: :controller do
     end
 
     it "responds redirect when we send request to /create" do
-      post :create, params: { email: "example@example.com", password: "example" }
+      post :create, params: params
       expect(response).to have_http_status "302"
     end
 
