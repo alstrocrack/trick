e302101e4e82d6cb758600dfbb58c25820be72b7
YutoUrushima <yuto259233@gmail.com>
2023-07-01 00:12:58 +0900

    Fix RegisterController spec

--- spec/controllers/register_controller_spec.rb
+++ spec/controllers/register_controller_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe RegisterController, type: :controller do
-  let(:user_account) { FactoryBot.create(:user_account) }
-  let(:user_session) { FactoryBot.create(:user_session, user_id: user_account.id) }
+  let(:user) { create(:user_account) }
+  let(:params) { { name: "example_user", email: "example@example.com", password: "example" } }
 
   context "as an guest user" do
     it "responds successfully" do
@@ -11,13 +11,17 @@ RSpec.describe RegisterController, type: :controller do
     end
 
     it "responds redirect when we send request to /create" do
-      post :create, params: { name: "example_user", email: "example@example.com", password: "example" }
+      post :create, params: params
       expect(response).to have_http_status "302"
     end
   end
 
   context "as an logined user" do
-    before { session[:user] = user_session.value }
+    let!(:user_session) { create(:user_session, user_id: user.id) }
+    before do
+      session[:user_id] = user.id
+      session[:user_token] = "exmaple-session-value"
+    end
 
     it "responds redirect when we visit /create page" do
       get :index
@@ -25,7 +29,7 @@ RSpec.describe RegisterController, type: :controller do
     end
 
     it "responds redirect when we send request to /create" do
-      post :create, params: { email: "example@example.com", password: "example" }
+      post :create, params: params
       expect(response).to have_http_status "302"
     end
   end
