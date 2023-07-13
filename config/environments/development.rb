e6db078ca8eb70b2232a6335e0b998308ec214ec
YutoUrushima <yuto259233@gmail.com>
2023-07-13 22:13:20 +0900

    Enable to protect CSRF in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Specifies whether CSRF protection is enabled
+  config.action_controller.allow_forgery_protection = true
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
